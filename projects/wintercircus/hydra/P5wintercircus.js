252b31840c530dceb414a48c53655df813857599
Kasper Jordaens <kasper.jordaens@gmail.com>
2024-09-05 08:34:35 +0200

    nvididatroubles

--- projects/wintercircus/hydra/P5wintercircus.js
+++ projects/wintercircus/hydra/P5wintercircus.js
@@ -1,49 +1,90 @@
+//////////////
+/// config ///
+//////////////
 
 
-function drawParticles(particles){
-    // console.log(particles[0].pos)
-    for (let i = 0; i < particles.length; i++) {
-        let part = particles[i];
-        part.update();
-        part.display();
-    
-        if (part.lifespan <= 0) {
-          particles.splice(i, 1);
-          i--;
-        }
-      }
-      
+function configP5(p5) {
+
+    // p5.lines = [];
+    // p5.rects = [];
+    // p5.imgs = [];
+
+    p5.cam = p5.createCamera();
+
+    // p5.updateit = false;
+    // p5.audioreact = false;
+    // p5.activeaudiobin = 0;
+ 
+    p5.viewportconf = {
+                      "rotX": 0,
+                      "rotY": 0,
+                      "rotZ":0,
+                      "rotSpeedX": 0, // 0.001,
+                      "rotSpeedY": 0, //0.001,
+                      "rotSpeedZ": 0, //0.0001,
+                      "offX":0,
+                      "offY":0,
+                      "offZ":0,
+                      "offSpeedX":0,
+                      "offSpeedY":0,
+                      "offSpeedZ":0,
+                      "cammode": "fixed", ///, "fixed" , "orbit","bigorbit"
+                      "audioCutoff": 1,
+                      "audioScale": 1,
+    }
+
+    p5.params = {  "data": {1:0.5,2:0.5,3:0.5,4:0.5}, 
+    "processed": {1:0.5,2:0.5,3:0.5,4:0.5}, 
+    "mask": {1:0,2:0,3:0,4:0},
+    "black": {1:0,2:0,3:0,4:0}, 
+    "audio":{1:0,2:0.8,3:0.6,4:0},
+    "viewportrot": {5:0.4,6:0.3,7:0.4}
+    }
+
+
 }
 
 
-class Particle {
-    constructor(x, y, z, txt, p) {
-      this.p5 = p
-      this.txt = txt
-      this.pos = this.p5.createVector(x, y, z);
-    //   this.vel = this.p5.createVector(this.p5.random(-10, 10), this.p5.random(2, 4),  this.p5.random(2, 4));
-      this.vel = this.p5.createVector(this.p5.random(-8, -4), 0, 0);
-      this.lifespan = 255;
-    }
-  
-    update() {
-      this.pos.add(this.vel);
-      if (this.pos.x < 10){
-        this.lifespan -= 5;
-      }
-    }
-  
-    display() {
-    //   console.log("drawing pat")
+function updateViewport(p5){
+  p5.viewportconf.rotX += p5.viewportconf.rotSpeedX;
+  p5.viewportconf.rotY += p5.viewportconf.rotSpeedY;
+  p5.viewportconf.rotZ += p5.viewportconf.rotSpeedZ;
+}
 
-    // this.p5.stroke(255, this.lifespan);
-    this.p5.fill(255, this.lifespan / 2);
-    this.p5.push()
-    this.p5.translate(this.pos.x, this.pos.y, this.pos.z)
-    // this.p5.box(20)
-    // this.p5.ellipse(0, 0, 16, 16);
-    this.p5.text(this.txt,0,0)
-    this.p5.pop()
 
-    }
+
+
+function updateCam(p5) {
+  if (p5.viewportconf.cammode == "fixed") {
+    p5.cam.camera(0, 0, 1000, 0, 0, 0, 0, 1, 0);
+  } else if (p5.viewportconf.cammode == "orbit") {
+    p5.cam.camera(p5.cos(p5.frameCount * 0.01) * 500, p5.sin(p5.frameCount * 0.01) * 500, p5.sin(p5.frameCount * 0.0029) * 300, 0, 0, 0, 0, 1, 0);
+  } else if (p5.viewportconf.cammode == "bigorbit") {
+    p5.cam.camera(p5.cos(p5.frameCount * 0.01) * 1000, p5.sin(p5.frameCount * 0.01) * 1000, p5.sin(p5.frameCount * 0.0029) * 1000, 0, 0, 0, 0, 1, 0);
   }
+}
+
+
+//////////////
+/// helpers //
+//////////////
+
+
+function random(p5) {
+  p5.cubesconfig.seed = p5.cubesconfig.seed +1
+  var x = Math.sin(p5.cubesconfig.seed++) * 10000;
+  return x - Math.floor(x);
+}
+
+function fakeRandom(seed) {
+  var x = Math.sin(seed) * 10000;
+  return x - Math.floor(x)
+}
+
+function localrandom(localseed) {
+  localseed = localseed +1
+  var x = Math.sin(localseed++) * 10000;
+  return x - Math.floor(x);
+}
+
+
--- projects/wintercircus/hydra/wintercircus.js
+++ projects/wintercircus/hydra/wintercircus.js
@@ -2,9 +2,15 @@
 
 // await loadScript("https://cdn.jsdelivr.net/gh/ojack/hydra-osc/lib/osc.min.js")
 // await loadScript("osc.min.js")
-await loadScript("extra/data.js")
 
 
+//midi config
+await loadScript("miditest.js")
+await midi.start({ channel: '*', input: '*' })
+tidal = midi.input(0) // live code data
+op1 = tidal // (no controller)
+onc = midi.input(3) // modular data
+midi.show()
 
 // render black
 solid(0)
@@ -28,57 +34,69 @@ a.setScale(5)
 a.beat.threshold = 10
 
 
-// vars
-hue = 0
-seed = 16777215
-shp = 5
-shapesize = 5
-b = 0;
-cubesize = 8;
+// // vars
+// hue = 0
+// seed = 16777215
+// shp = 5
+// shapesize = 5
+// b = 0;
+// cubesize = 8;
 
-xs = 30;
-margin = 100;
-speed = 0;
-value = 100;
-par = "speed";
-a.onBeat = () => {
-	b += 1;
-}
+// xs = 30;
+// margin = 100;
+// speed = 0;
+// value = 100;
+// par = "speed";
+// a.onBeat = () => {
+// 	b += 1;
+// }
 
 
 
 
 // synth0 - ascii art
 // s0.initScreen();
-src(solid(244,34,123))
+solid(244,34,123)
 	.out(o0);
+render()
 
 // synth1 - tidalcode
 // s1.initScreen();
 // src(s1)
-src(solid(244,255,0))
+(244,255,0)
 	.out(o1);
 
 // synth2 - images
 // s2.initImage("img/" + img[0]);
 src(s2).out(o2);
 
-// synth3 - processing
+// // synth3 - processing
 p = new P5({
+	height: rw, 
+	width: rh, 
 	mode: "WEBGL"
 })
-
-p.hide()
 
 s3.init({
 	src: p.canvas
 })
-p.myFont = p.loadFont('extra/PressStart2P-Regular.otf');
-p.textFont(p.myFont);
-p.particles = [];
+
+p.hide()
 
 
-await loadScript("extra/P5wintercircus.js") // loads extra p5 functions
+// await loadScript("hydra/P5wintercircus.js") // loads p5 helpper functions
+// await loadScript("hydra/P5cubes.js") // loads cubes 
+// await loadScript("hydra/images.js") // loads images data
+// await loadScript("hydra/P5images.js") // loads images
+// await loadScript("hydra/P5lines.js") // loads images
+// await loadScript("hydra/P5rects.js") // loads rects	
+// await loadScript("hydra/data.js") // loads text data
+// await loadScript("hydra/P5textparticles.js") // loads text particles
+// await loadScript("hydra/extraHydra.js") // loads hydrapresets
+// await loadScript("hydra/extraMidi.js") // loads midi
+
+
+
 
 // p.preload = () => {
   
@@ -89,56 +107,84 @@ await loadScript("extra/P5wintercircus.js") // loads extra p5 functions
 // }
 
 
+configP5(p)
+createCubes(p)
+// initImgs(p)
+// preloadImg(p,maskssmall)
+// initRects(p)
+// initLines(p)
+// initTextParts(p)
+// initMidi(p)
+
 // p.draw = () => {}
-f = 0
+// f = 0
 p.draw = () => {
-  	
- 	p.background(0);
-  	
-  	if (p.particles.length < 50){
-    // Add new particles
-      for (let i = 0; i < 2; i++) {
-        f++;
-        let x = width;
-        let y = (f%50)*36;
-        let z = 0-(f%50)*y/144;
-        if (f%50 < 25) {
-        	z = (f%50)*36;
-        }
-        let txt = techTerms[Math.floor(Math.random() * techTerms.length)];
-        let part = new Particle(x, y, z, txt, p);
-        p.particles.push(part);
-      }
-    }
-  	p.textSize(36);
+	p.background(0);
 	p.push();
-    for (var i = 0; i < techTerms.length; i++) {
-//       p.text(techTerms[i], 10, 20*i);
-	}
-// 	p.rotateX(p.frameCount * 0.1 );
-//   	p.translate(0,p.frameCount%height )
-// 	p.rotateY(p.frameCount * 0.01 );
-// 	p.rotateZ(p.frameCount * 0.031 );
-// 	p.box(300)
-//     p.fill('#ED225D');
-  p.translate(-width/2, -height -100 )
-  	drawParticles(p.particles)
-//   	
-//   	p.text('DMG - museumnacht', 10, 50);
+		updateViewport (p);
+		updateCam(p);
+		// updateParticles(p)
+		// drawParticles(p)
+		updateCubes(p);
+		renderCubes(p);
+		// updateLines(p);
+		// renderLines(p)
+		// updateImgs(p);
+		// renderImgs(p);
+		// updateRects(p);
+		// renderRects(p);
   	p.pop();
 }
 
 
-src(s3)
-// .mult(osc(1, -0.1).modulate(osc(2).rotate(()=> 4,1), 20))
-// .luma(1,0.1, (6, ()=> 1 + a.fft[3]))
-// .scale(0.7, ()=> 0.7 + a.fft[3])
-.color(1.04,0, -1.1).pixelate(200, 20).modulate(noise(2.5), () => 1.5 * Math.sin(0.08 * time))
-.blend(s3)
-.out(o3);
+// src(o2)
+// // .mult(osc(1, -0.1).modulate(osc(2).rotate(()=> 4,1), 20))
+// // .luma(1,0.1, (6, ()=> 1 + a.fft[3]))
+// // .scale(0.7, ()=> 0.7 + a.fft[3])
+// .color(1.04,0, -1.1).pixelate(200, 20).modulate(noise(() => a.fft[3]*2.5), () => 1.5 * Math.sin(0.08 * time))
+// // .blend(s3, () => p.oncdata4/127)
+// .blend(o2, 0.4)
+// .kaleid(5)
+// .out(o3);
 
 
 
-render(o3);
+// shape(2,0.1,0.1)
+// //   .scale(() => Math.sin(time)*a.fft[0]*40)
+// //   .repeat(() => Math.sin(time)*10)
+// //   .repeat(() => 100)
+//   .modulateRotate(o2)
+//   .scale(() => p.oncdata1/127 + 0.1)
+// //   .modulate(noise(2,({time}) => Math.sin(time*0.05)*0.05 ))
+// //   .rotate(() => p.oncdata4/127 + 100, 0.9)
+// //   .scrollY(0, ({time}) => Math.sin(time*0.05)*0.05 )
+// .out(o0)
+
+// src(o0)
+// .modulate(osc(5,0,0))
+// // .modulateRepeatX(osc(10), 5.0, ({time}) => Math.sin(time) * 5)
+// // .modulateRepeatX(osc(12), 500.0, ({time}) => Math.sin(time) * 5)
+// .out(o1)
+
+// src(o1)
+// // .modulateKaleid(voronoi(() => Math.sin(time)*3,	0.1,0.01),() => Math.sin(time)*3)
+// .scale(() => Math.sin(time)*30)
+// .out(o2)
 
 
+// shape(20,0.1,0.01)
+//   .scale(() => Math.sin(time)*2)
+//   .modulate(noise(2,0))
+// .out(o0)
+
+// render(o2)
+
+// src(o0)
+// .blend(o2, () => p.oncdata4/255 - 0.1)
+// .out(o3)
+
+
+
+render(o3);	
+
+render();
