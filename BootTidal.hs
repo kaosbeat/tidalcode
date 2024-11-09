c33f83809d302fe89c91518b75701f0407a03e20
Kasper Jordaens <kasper.jordaens@gmail.com>
2024-11-09 13:18:51 +0000

    config

--- BootTidal.hs
+++ BootTidal.hs
@@ -1,1 +1,1 @@
-/home/kaos/.cabal/store/ghc-9.0.2/tidal-1.9.4-deefedab3084f37052b90cca25906d4ac87389bea3f7694ad9f719f3d84aac30/share/BootTidal.hs
+/home/patch/.cabal/store/ghc-9.0.2/tidal-1.9.5-28f3161c9b108348ab5ae779a7d8525860297d7cbbcb71a5cd090929ee20f312/share/BootTidal.hs
